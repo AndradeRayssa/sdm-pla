5a62d5298a7c8ffba3b1d9bcd32ffacbead65e5f
Rayssa dos Santos Chimentão Andrade <824122332@ulife.com.br>
2025-08-22 11:49:54 -0300

    Trabalhando com funções em js

---
+++ funcoes.js
@@ -0,0 +1,27 @@
+// Função declarativa
+function saudacao(nome) {
+    return `Olá, ${nome}!`
+}
+
+console.log(saudacao("Rayssa"))
+
+
+// Função anônima
+const soma = function(a,b) {
+    return a + b;
+}
+
+function simples(){
+let a = 5
+let b = 12
+let c = a + b
+
+}
+console.log("O valor de c é: " + c)
+
+function somar (a,b){
+    return a + b;
+}
+
+console.log( somar(5,10))
+
