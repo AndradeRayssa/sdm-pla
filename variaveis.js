6eb3f75154e978d23075102082ffc04588ab8aab
Rayssa dos Santos Chimentão Andrade <824122332@ulife.com.br>
2025-08-22 11:13:41 -0300

    Conhecendo variáveis em JavaScript

---
+++ variaveis.js
@@ -0,0 +1,46 @@
+var nome = "Rayssa"
+const altura = 1.65
+let idade = 19
+//altura = 1.70
+idade = 20
+
+console.log("Meu nome é " + nome + ", minha altura é " + altura + " e minha idade é " + idade + " anos.")
+
+// String
+let texto = "String"
+
+//Number
+let numero = 10
+let numero2 = 10.5
+
+// boolean
+let booleano = true
+let booleano2 = false
+
+// null
+let nulo = null
+
+// undefined
+let indefinido = undefined
+
+// object
+let obejto = {nome:"Rayssa", idade: 19, altura: 1.65}
+
+// array
+lista = ["uva", "maçã", "morango"]
+
+
+//Conversão de tipos de dados
+
+// String -> Números
+let stringParaNumeo = Number("10")
+let stringParaNumero2 = parseInt("10.5") // converte para inteiro
+let stringParaMunero3 = parseFloat("10.5") // converte para float
+
+// Número -> String
+let str = String(100);
+let str2 = (100).toString();
+
+// Boolean -> Número
+let booleanParaNumero = Number(true); // 1
+let booleanParaNumero2 = Number(false); // 0
