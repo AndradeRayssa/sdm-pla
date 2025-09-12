c1b8b5c4e6c03ff02bc73473c644deca4c121d15
Rayssa dos Santos Chimentão Andrade <824122332@ulife.com.br>
2025-09-12 11:53:13 -0300

    Adicionado order e user service

---
+++ user-service/userService.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+
+//post
+app.post('/usuarios', async (req, res) => {
+    const usuario = req.body;
+
+    await axios.post('http://localhost:4000/pedidos', {userId: usuario.id});
+
+    res.send({message: 'Usuário criado com sucesso!', usuario})
+});
+
+app.listen(3000, () => {
+    console.log('User Service rodando na porta 3000')
+});
